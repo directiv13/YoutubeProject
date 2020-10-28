2fcb2f2c276f31459921ef64472cf2513c684001
Vlad <vladpoleshchuk@gmail.com>
2020-10-29 00:32:16 +0200

    Ready YoutubeSearch.GetThumbnails() and YoutubeSearch.GetSearchResult(). Made them by parsing;

--- YoutubeProject/YoutubeSearch.cs
+++ YoutubeProject/YoutubeSearch.cs
@@ -1,36 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using HtmlAgilityPack;
+using Jurassic.Library;
+using Newtonsoft.Json.Linq;
 
 namespace YoutubeProject
 {
     class YoutubeSearch:IScrapper
     {
-        readonly string thumbnailPattern = @"(\s*\W)videoRenderer(\W):\s*{(\W)videoId(\W):(\W)(\S*)(\W),(\s*\W)thumbnail(\W):\s*{(\W)thumbnails(\W\s*):\s*\[{(\W)url(\W):(\s*\W)(?<URL>\S*)(\W),(\s*\W)width(\W):\d*,(\s*\W)height(\W):\d*}";
-        readonly string videoPattern = @"{\u0022url\u0022:\u0022\/watch\?v=(?<VIDEO>\S*)\u0022,\u0022webPageType\u0022:\u0022WEB_PAGE_TYPE_WATCH\u0022,\u0022rootVe\u0022:3832}}";
-        readonly string titlePattern = @"\u0022title\u0022:\W*{\u0022runs\u0022:\W*\[{\u0022text\u0022:\W*\u0022(?<TITLE>.+?(?=(\u0022}\],)))";
-
-        public string GetSearchResult(string searchRequset)
+        public string GetSearchResult(string searchRequest)
         {
-            throw new NotImplementedException();
+            try
+            {
+                String htmlQuery;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.youtube.com/results?search_query=" + searchRequest);
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        htmlQuery = reader.ReadToEnd();
+                }
+
+                var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                htmlDocument.LoadHtml(htmlQuery);
+
+                List<HtmlNode> scriptTags = htmlDocument.DocumentNode.Descendants()
+                             .Where(n => n.Name == "script").ToList();
+
+                var engine = new Jurassic.ScriptEngine();
+                var result = engine.Evaluate("(function() { " + scriptTags[28].InnerText + " return ytInitialData; })()");
+                string json = JSONObject.Stringify(engine, result);
+
+                return json;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
+                return null;
+            }
         }
 
-        public List<string> GetThumbnails(string htmlQuery)
+        public List<string> GetThumbnails(string json)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var jObject = JObject.Parse(json);
+
+                var contents = jObject.SelectToken("$.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents[0]");
+
+                List<string> thumbnailURLs = new List<string>(6);
+                for (int i = 0; i < 6; i++)
+                {
+                    thumbnailURLs.Add(contents.SelectToken("$.videoRenderer.thumbnail.thumbnails[0].url").Value<string>());
+                    contents = contents.Next;
+                }
+                return thumbnailURLs;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
+                return null;
+            }
         }
 
         public List<string> GetTitles(string htmlQuery)
         {
-            throw new NotImplementedException();
+            try 
+            {
+                return null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
+                return null;
+            }  
         }
 
         public List<string> GetVideoId(string htmlQuery)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
+                return null;
+            }
         }
     }
 }
