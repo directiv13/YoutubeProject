ef39494531285cebf87c249c620c45ab44a3d96e
Vlad <vladpoleshchuk@gmail.com>
2020-12-04 15:57:50 +0300

    IDisposable fix on IUserManager Interface

--- BusinessLogic/Security/IUserManager.cs
+++ BusinessLogic/Security/IUserManager.cs
@@ -1,9 +1,10 @@
 ﻿using BusinessLogic.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace BusinessLogic.Security
 {
-    public interface IUserManager
+    public interface IUserManager : IDisposable
     {
         Task<UserOpResult> TryLoginAsync(LoginUserDto user);
         Task<UserOpResult> TrySignUpAsync(CreateUserDto user);
